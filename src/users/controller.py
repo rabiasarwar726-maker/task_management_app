2b04d6c92cc605f04da939171678ef8c777d41ee
rabiasarwar726-maker <rsarwar726@gmail.com>
2026-04-05 23:55:31 +0500

    i added jwt token generation to the login function.

--- src/users/controller.py
+++ src/users/controller.py
@@ -3,8 +3,14 @@ from src.users.dtos import UserSchema,LoginSchema
 from sqlalchemy.orm import Session
 from src.users.models import UserModel
 from pwdlib import PasswordHash
+from src.utils.settings import settings
+from datetime import datetime, timedelta
+import jwt
 
 password_hasher = PasswordHash.recommended()
+
+jwt_secret = settings.SECRET_KEY
+jwt_algorithm = settings.ALGORITHM
 
 def get_password_hash(password):
   return password_hasher.hash(password)
@@ -34,4 +40,8 @@ def login_user(body:LoginSchema,db:Session):
     raise HTTPException(status_code=400, detail="Invalid username or password")
   if not password_hasher.verify(body.password, user.hash_password):
     raise HTTPException(status_code=400, detail="Invalid username or password")
+  exp_time = datetime.now() + timedelta(minutes=settings.EXP_TIME)
+  token=jwt.encode({"_id": user.id,"exp": exp_time}, settings.SECRET_KEY, settings.ALGORITHM)
+  
+  return{"token":token}
   return{"message": "Login successful", "user": user}
